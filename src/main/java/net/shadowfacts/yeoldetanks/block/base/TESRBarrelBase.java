72f0fa512becef16d553d4f182484f6433bede93
Shadowfacts <me@shadowfacts.net>
2017-03-25 16:01:25 -0400

    Animated fluid texture in barrels Closes #5

--- src/main/java/net/shadowfacts/yeoldetanks/block/base/TESRBarrelBase.java
+++ src/main/java/net/shadowfacts/yeoldetanks/block/base/TESRBarrelBase.java
@@ -1,46 +1,72 @@
 package net.shadowfacts.yeoldetanks.block.base;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
+import net.shadowfacts.shadowmc.util.RenderHelper;
 import net.shadowfacts.yeoldetanks.YOTConfig;
-import net.shadowfacts.yeoldetanks.block.creativebarrel.TileEntityCreativeBarrel;
-import net.shadowfacts.yeoldetanks.client.model.ModelFluid;
-import net.shadowfacts.yeoldetanks.client.render.RenderUtils;
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author shadowfacts
  */
 public class TESRBarrelBase extends TileEntitySpecialRenderer<TileEntityBarrelBase> {
 
-	private ModelFluid modelFluid = new ModelFluid();
-
 	@Override
 	public void renderTileEntityAt(TileEntityBarrelBase barrel, double x, double y, double z, float partialTicks, int destroyStage) {
-		if (YOTConfig.renderFluid && barrel.getTank().getFluid() != null) {
-			ResourceLocation fluidTexture = RenderUtils.getTexture(barrel.getTank().getFluid().getFluid());
+		if (YOTConfig.renderFluid && barrel.getTank().getFluid() != null && barrel.getTank().getFluidAmount() > 0) {
+			FluidStack fluid = barrel.getTank().getFluid();
+			ResourceLocation loc = fluid.getFluid().getStill(barrel.getTank().getFluid());
+			TextureAtlasSprite sprite = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(loc.toString());
 
-			if (fluidTexture != null) {
-				GlStateManager.pushMatrix();
+			GlStateManager.pushMatrix();
 
-				GlStateManager.translate((float)x + .5f, (float)y + 1.5f, (float)z + .5f);
-				GlStateManager.rotate(180, 1, 0, 0);
+			GlStateManager.disableLighting();
+			GlStateManager.enableBlend();
+			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-				float fluidPercent;
-				if (barrel.isCreative()) {
-					fluidPercent = 1;
-				} else {
-					fluidPercent = (float)barrel.getTank().getFluidAmount() / barrel.getTank().getCapacity();
-				}
-				if (YOTConfig.blameEllpeck) fluidPercent *= -1;
-				GlStateManager.translate(0, fluidPercent * -0.9f, 0);
-
-				bindTexture(fluidTexture);
-				modelFluid.renderAll();
-
-				GlStateManager.popMatrix();
+			if (Minecraft.isAmbientOcclusionEnabled()) {
+				GL11.glShadeModel(GL11.GL_SMOOTH);
+			} else {
+				GL11.glShadeModel(GL11.GL_FLAT);
 			}
+
+			GlStateManager.translate(x, y, z);
+
+			VertexBuffer buf = Tessellator.getInstance().getBuffer();
+			buf.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+
+			Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+			int color = fluid.getFluid().getColor(fluid);
+			int brightness = Minecraft.getMinecraft().world.getCombinedLight(barrel.getPos(), fluid.getFluid().getLuminosity(fluid));
+
+			double fluidPercent = barrel.isCreative() ? 1 : (double)fluid.amount / barrel.getTank().getCapacity();
+			double fluidY = fluidPercent * 29/32d + 1/16d;
+
+			// Center
+			RenderHelper.putTexturedQuad(buf, sprite, 2/16d, fluidY, 2/16d, 12/16d, 0, 12/16d, EnumFacing.UP, color, brightness);
+			// Top
+			RenderHelper.putTexturedQuad(buf, sprite, 3/16d, fluidY, 1/16d, 10/16d, 0, 1/16d, EnumFacing.UP, color, brightness);
+			// Bottom
+			RenderHelper.putTexturedQuad(buf, sprite, 3/16d, fluidY, 14/16d, 10/16d, 0, 1/16d, EnumFacing.UP, color, brightness);
+			// Left
+			RenderHelper.putTexturedQuad(buf, sprite, 1/16d, fluidY, 3/16d, 1/16d, 0, 10/16d, EnumFacing.UP, color, brightness);
+			// Right
+			RenderHelper.putTexturedQuad(buf, sprite, 14/16d, fluidY, 3/16d, 1/16d, 0, 10/16d, EnumFacing.UP, color, brightness);
+
+			Tessellator.getInstance().draw();
+
+			GlStateManager.disableBlend();
+			GlStateManager.enableLighting();
+			GlStateManager.popMatrix();
 		}
 	}
 
--- src/main/java/net/shadowfacts/yeoldetanks/client/render/minecart/RenderBarrelMinecart.java
+++ src/main/java/net/shadowfacts/yeoldetanks/client/render/minecart/RenderBarrelMinecart.java
@@ -1,21 +1,27 @@
 package net.shadowfacts.yeoldetanks.client.render.minecart;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderMinecart;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
+import net.shadowfacts.shadowmc.util.RenderHelper;
 import net.shadowfacts.yeoldetanks.YOTConfig;
-import net.shadowfacts.yeoldetanks.client.model.ModelFluid;
-import net.shadowfacts.yeoldetanks.client.render.RenderUtils;
 import net.shadowfacts.yeoldetanks.entity.barrelminecart.EntityBarrelMinecart;
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author shadowfacts
  */
 public class RenderBarrelMinecart extends RenderMinecart<EntityBarrelMinecart> {
-
-	private ModelFluid modelFluid = new ModelFluid();
 
 	public RenderBarrelMinecart(RenderManager renderManager) {
 		super(renderManager);
@@ -26,24 +32,50 @@ public class RenderBarrelMinecart extends RenderMinecart<EntityBarrelMinecart> {
 		super.renderCartContents(minecart, partialTicks, state);
 
 		if (YOTConfig.renderFluid && minecart.tank.getFluid() != null && minecart.tank.getFluidAmount() > 0) {
-			ResourceLocation fluidTexture = RenderUtils.getTexture(minecart.tank.getFluid().getFluid());
+			FluidStack fluid = minecart.tank.getFluid();
+			ResourceLocation loc = fluid.getFluid().getStill(minecart.tank.getFluid());
+			TextureAtlasSprite sprite = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(loc.toString());
 
-			if (fluidTexture != null) {
+			GlStateManager.pushMatrix();
 
-				GlStateManager.pushMatrix();
+			GlStateManager.disableLighting();
+			GlStateManager.enableBlend();
+			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-				GlStateManager.translate(0.5f, 1.5f, -0.5f);
-				GlStateManager.rotate(180, 1, 0, 0);
-
-				float fluidPercent = -(float)minecart.tank.getFluidAmount() / minecart.tank.getCapacity();
-				GlStateManager.translate(0, fluidPercent * 0.9f, 0);
-
-
-				bindTexture(fluidTexture);
-				modelFluid.renderAll();
-
-				GlStateManager.popMatrix();
+			if (Minecraft.isAmbientOcclusionEnabled()) {
+				GL11.glShadeModel(GL11.GL_SMOOTH);
+			} else {
+				GL11.glShadeModel(GL11.GL_FLAT);
 			}
+
+			GlStateManager.translate(0, 0, -1);
+
+			VertexBuffer buf = Tessellator.getInstance().getBuffer();
+			buf.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+
+			Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+			int color = fluid.getFluid().getColor(fluid);
+			int brightness = Minecraft.getMinecraft().world.getCombinedLight(minecart.getPosition(), fluid.getFluid().getLuminosity(fluid));
+
+			double fluidPercent = (double)fluid.amount / minecart.tank.getCapacity();
+			double fluidY = fluidPercent * 29/32d + 1/16d;
+
+			// Center
+			RenderHelper.putTexturedQuad(buf, sprite, 2/16d, fluidY, 2/16d, 12/16d, 0, 12/16d, EnumFacing.UP, color, brightness);
+			// Top
+			RenderHelper.putTexturedQuad(buf, sprite, 3/16d, fluidY, 1/16d, 10/16d, 0, 1/16d, EnumFacing.UP, color, brightness);
+			// Bottom
+			RenderHelper.putTexturedQuad(buf, sprite, 3/16d, fluidY, 14/16d, 10/16d, 0, 1/16d, EnumFacing.UP, color, brightness);
+			// Left
+			RenderHelper.putTexturedQuad(buf, sprite, 1/16d, fluidY, 3/16d, 1/16d, 0, 10/16d, EnumFacing.UP, color, brightness);
+			// Right
+			RenderHelper.putTexturedQuad(buf, sprite, 14/16d, fluidY, 3/16d, 1/16d, 0, 10/16d, EnumFacing.UP, color, brightness);
+
+			Tessellator.getInstance().draw();
+
+			GlStateManager.disableBlend();
+			GlStateManager.enableLighting();
+			GlStateManager.popMatrix();
 		}
 	}
 
